3983ff1c4c7d71688d7c23fecf1eac08554b75f2
Sean Sweet <sweet.sean42@gmail.com>
2025-04-09 21:24:19 -0500

    initial http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+use rocket::{
+    get,
+    http::Status,
+    launch, routes,
+    serde::{json::Json, Serialize},
+};
+
+#[derive(Serialize)]
+struct TempuratureResponse {
+    tempurature: i32,
+    id: String,
+    name: String,
+    connected: bool,
+}
+
+#[get("/tempurature")]
+fn get_tempurature() -> Result<Json<TempuratureResponse>, Status> {
+    let Ok(tempurature_string) = fs::read_to_string("/dev/thermometer") else {
+        return Err(Status::NoContent);
+    };
+
+    let Ok(tempurature) = tempurature_string.trim().parse() else {
+        return Err(Status::InternalServerError);
+    };
+
+    Ok(Json(TempuratureResponse {
+        tempurature,
+        id: "sensor01".to_owned(),
+        name: "office".to_owned(),
+        connected: true,
+    }))
+}
+
+#[launch]
+fn tempurature_server() -> _ {
+    rocket::build().mount("/", routes![get_tempurature])
 }
--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-result="$(cargo run)"
+cargo run &
 
-if [ "$result" == "Hello, world!" ]; then
+expected_result='{"tempurature":70,"id":"sensor01","name":"office","connected":true}'
+result=$(curl -X GET "http://127.0.0.1:8000/tempurature")
+
+if [ "$result" == "$expected_result" ]; then
     echo "Test Passed"
 else
-    echo "Test Failed: Expected \"Hello, world!\", got $result"
+    echo "Test Failed: Expected $expected_result, got $result"
     exit 1
 fi
